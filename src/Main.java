1cf27306810c4b3e99902541c4d1067fd0f5a696
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-27 09:37:09 -0400

    birth

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int birth = 0;
+        System.out.println("Please enter your birth month:");
+        birth = scan.nextInt();
+        if (1 <= birth && birth <= 12) {
+            System.out.println("Your birth month is: " + birth);
+        }
+        else if (1 > birth || birth > 12) {
+            System.out.println("You have entered an invalid birth month");
+            System.exit(0);
+        }
+
+
     }
 }
